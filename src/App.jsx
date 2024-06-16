c87b8b30e0aebef7f90c59772865257c976911d4
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-16 19:44:42 +0200

    remove leftover imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import CreateCustomer from "./CreateCustomer";
-import Customer from "./Customer";
-import AccountOperations from "./AccountOperations";
-import BalanceDisplay from "./BalanceDisplay";
-
 function App() {
   return (
     <div>
